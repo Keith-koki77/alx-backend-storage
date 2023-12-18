2758cdc281738764180bcbdd0d68de9fb29c106a
Keith koki <kokikeith@gmail.com>
2023-12-18 12:24:03 -0800

    file fixed

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,4 +5,5 @@ Return the list of schools having a specific topic.
 
 
 def schools_by_topic(mongo_collection, topic):
+    """Returns:A list of school documents matching the specified topic."""
     return mongo_collection.find({"topics": topic})
