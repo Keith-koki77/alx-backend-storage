80d9d64ea118649c268234c5b3a544a264a1aefe
Keith koki <kokikeith@gmail.com>
2023-12-18 12:19:03 -0800

    file fixed

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,4 +5,5 @@ Update topics of a school document based on the name.
 
 
 def update_topics(mongo_collection, name, topics):
+    """Returns:The number of documents updated."""
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
