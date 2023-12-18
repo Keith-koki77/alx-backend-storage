9704055fd18061aa5c8d038d41310fbc9d3ed32e
Keith koki <kokikeith@gmail.com>
2023-12-18 12:10:57 -0800

    file fixed

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,5 +5,5 @@ Insert a new document into a MongoDB collection based on keyword arguments.
 
 
 def insert_school(mongo_collection, **kwargs):
-    result = mongo_collection.insert_one(kwargs)
-    return result.inserted_id
+    documents = mongo_collection.insert_one(kwargs)
+    return documents.inserted_id
